eebb78f6538bef641fc287f7093c1e3e8ad6d579
khoinguyen <vankhoi06102002@gmail.com>
2024-05-14 23:03:15 +0700

    upadte

---
+++ app/src/main/java/com/kan/dev/familyhealth/ui/activity/InformationActivity.kt
@@ -0,0 +1,29 @@
+package com.kan.dev.familyhealth.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kan.dev.familyhealth.R
+import com.kan.dev.familyhealth.base.BaseActivity
+import com.kan.dev.familyhealth.databinding.ActivityInformationBinding
+
+class InformationActivity : BaseActivity<ActivityInformationBinding>() {
+    override fun setViewBinding(): ActivityInformationBinding {
+        return ActivityInformationBinding.inflate(layoutInflater)
+    }
+
+    override fun initData() {
+        
+    }
+
+    override fun initView() {
+        
+    }
+
+    override fun initListener() {
+        
+    }
+
+}
